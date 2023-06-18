e06f7fe110517b91ab5a55251ce684dcf9a2cb77
mat1jaczyyy <mat1jaczyyy@gmail.com>
2023-06-19 01:58:37 +0200

    Source: Get name from CfgMgr32 for non-USB devices

--- Keyboard Inspector/Recording/Native.cs
+++ Keyboard Inspector/Recording/Native.cs
@@ -608,5 +608,79 @@ namespace Keyboard_Inspector {
 
         [DllImport("hid.dll", CharSet = CharSet.Auto, SetLastError = true)]
         public static extern bool HidD_GetProductString(IntPtr hFile, StringBuilder buffer, int bufferLength);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct DEVPROPKEY {
+            public Guid fmtid;
+            public uint pid;
+        }
+
+        public static DEVPROPKEY DEVPKEY_Device_InstanceId = new DEVPROPKEY() {
+            fmtid = new Guid(0x78c34fc8, 0x104a, 0x4aca, 0x9e, 0xa4, 0x52, 0x4d, 0x52, 0x99, 0x6e, 0x57),
+            pid = 256
+        };
+
+        public static DEVPROPKEY DEVPKEY_Device_BusTypeGuid = new DEVPROPKEY() {
+            fmtid = new Guid(0xa45c254e, 0xdf1c, 0x4efd, 0x80, 0x20, 0x67, 0xd1, 0x46, 0xa8, 0x50, 0xe0),
+            pid = 21
+        };
+
+        public static DEVPROPKEY DEVPKEY_NAME = new DEVPROPKEY() {
+            fmtid = new Guid(0xb725f130, 0x47ef, 0x101a, 0xa5, 0xf1, 0x02, 0x60, 0x8c, 0x9e, 0xeb, 0xac),
+            pid = 10
+        };
+
+        public static readonly uint DEVPROP_TYPE_STRING = 0x12;
+        public static readonly uint DEVPROP_TYPE_GUID = 0x0D;
+
+        public static readonly int CR_SUCCESS = 0x00;
+        public static readonly int CR_BUFFER_SMALL = 0x1A;
+
+        [DllImport("CfgMgr32.dll", CharSet = CharSet.Unicode)]
+        public static extern int CM_Get_Device_Interface_Property(
+            string pszDeviceInterface,
+            ref DEVPROPKEY PropertyKey,
+            out uint PropertyType,
+            StringBuilder PropertyBuffer,
+            ref int PropertyBufferSize,
+            uint ulFlags
+        );
+
+        [Flags()]
+        public enum Locate_DevNode_Flags {
+            CM_LOCATE_DEVNODE_NORMAL = 0,
+            CM_LOCATE_DEVNODE_PHANTOM = 1,
+            CM_LOCATE_DEVNODE_CANCELREMOVE = 2,
+            CM_LOCATE_DEVNODE_NOVALIDATION = 4
+        }
+
+        [DllImport("CfgMgr32.dll", CharSet = CharSet.Unicode)]
+        public static extern int CM_Locate_DevNode(
+            out int pdnDevInst,
+            string pDeviceID,
+            Locate_DevNode_Flags ulFlags
+        );
+
+        [DllImport("CfgMgr32.dll", CharSet = CharSet.Unicode)]
+        public static extern int CM_Get_DevNode_Property(
+            int dnDevInst,
+            ref DEVPROPKEY PropertyKey,
+            out uint PropertyType,
+            StringBuilder PropertyBuffer,
+            ref int PropertyBufferSize,
+            uint ulFlags
+        );
+
+        [DllImport("CfgMgr32.dll", CharSet = CharSet.Unicode)]
+        public static extern int CM_Get_DevNode_Property(
+            int dnDevInst,
+            ref DEVPROPKEY PropertyKey,
+            out uint PropertyType,
+            ref Guid PropertyBuffer,
+            ref int PropertyBufferSize,
+            uint ulFlags
+        );
+
+        public static Guid GUID_BUS_TYPE_HID = new Guid(0xeeaf37d0, 0x1963, 0x47c4, 0xaa, 0x48, 0x72, 0x47, 0x6d, 0xb7, 0xcf, 0x49);
     }
 }
--- Keyboard Inspector/Result/Source.cs
+++ Keyboard Inspector/Result/Source.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using static Keyboard_Inspector.Native;
 using System.Linq;
 using System.Text;
 
@@ -17,27 +16,116 @@ namespace Keyboard_Inspector {
             DeviceInterface = device_interface;
         }
 
-        public static Source FromHandle(long handle, int count) {
-            IntPtr hDevice = new IntPtr(handle);
+        static bool CfgMgrName(string interface_name, out string name, out string instance_name) {
+            name = null;
+            instance_name = null;
 
-            if (!GetRawInputDeviceInfo(hDevice, RawInputCommand.DeviceName, out byte[] interface_bytes)) return null;
+            uint type;
+            int size = 0;
 
-            var device_interface = Encoding.UTF8.GetString(interface_bytes.TakeWhile(i => i != 0).ToArray());
+            int ret = Native.CM_Get_Device_Interface_Property(
+                interface_name,
+                ref Native.DEVPKEY_Device_InstanceId,
+                out type,
+                null, ref size, 0
+            );
+            if (ret != Native.CR_BUFFER_SMALL || type != Native.DEVPROP_TYPE_STRING) return false;
 
+            StringBuilder buffer = new StringBuilder(size);
+
+            ret = Native.CM_Get_Device_Interface_Property(
+                interface_name,
+                ref Native.DEVPKEY_Device_InstanceId,
+                out type,
+                buffer, ref size, 0
+            );
+            if (ret != Native.CR_SUCCESS || type != Native.DEVPROP_TYPE_STRING) return false;
+
+            instance_name = buffer.ToString();
+
+            ret = Native.CM_Locate_DevNode(out int devInst, instance_name, Native.Locate_DevNode_Flags.CM_LOCATE_DEVNODE_PHANTOM);
+            if (ret != 0) return false;
+
+            size = 0;
+
+            ret = Native.CM_Get_DevNode_Property(
+                devInst,
+                ref Native.DEVPKEY_Device_BusTypeGuid,
+                out type,
+                null, ref size, 0
+            );
+            if (ret != Native.CR_BUFFER_SMALL || type != Native.DEVPROP_TYPE_GUID) return false;
+
+            Guid guid = new Guid();
+
+            ret = Native.CM_Get_DevNode_Property(
+                devInst,
+                ref Native.DEVPKEY_Device_BusTypeGuid,
+                out type,
+                ref guid, ref size, 0
+            );
+            if (ret != Native.CR_SUCCESS || type != Native.DEVPROP_TYPE_GUID) return false;
+
+            if (guid == Native.GUID_BUS_TYPE_HID) return false;
+
+            size = 0;
+
+            ret = Native.CM_Get_DevNode_Property(
+                devInst,
+                ref Native.DEVPKEY_NAME,
+                out type,
+                null, ref size, 0
+            );
+            if (ret != Native.CR_BUFFER_SMALL || type != Native.DEVPROP_TYPE_STRING) return false;
+
+            buffer = new StringBuilder(size);
+
+            ret = Native.CM_Get_DevNode_Property(
+                devInst,
+                ref Native.DEVPKEY_NAME,
+                out type,
+                buffer, ref size, 0
+            );
+            if (ret != Native.CR_SUCCESS || type != Native.DEVPROP_TYPE_STRING) return false;
+
+            name = buffer.ToString();
+            return true;
+        }
+
+        static bool HIDProductName(string interface_name, out string name) {
             var productString = new StringBuilder(2000);
+            name = null;
 
-            var hFile = CreateFile(device_interface, 0, 3, IntPtr.Zero, 3, 0, IntPtr.Zero);
+            var hFile = Native.CreateFile(interface_name, 0, 3, IntPtr.Zero, 3, 0, IntPtr.Zero);
             if (hFile != IntPtr.Zero) {
                 try {
-                    if (!HidD_GetProductString(hFile, productString, productString.Capacity))
-                        return null;
+                    if (!Native.HidD_GetProductString(hFile, productString, productString.Capacity))
+                        return false;
 
                 } finally {
-                    CloseHandle(hFile);
+                    Native.CloseHandle(hFile);
                 }
             }
 
-            return new Source(count, productString.ToString(), device_interface);
+            name = productString.ToString();
+            return true;
+        }
+
+        public static Source FromHandle(long handle, int count) {
+            string name = "Unknown device";
+            string interface_name = $"hDevice=0x{handle:X08}";
+
+            if (Native.GetRawInputDeviceInfo(new IntPtr(handle), Native.RawInputCommand.DeviceName, out byte[] interface_bytes)) {
+                interface_name = Encoding.UTF8.GetString(interface_bytes.TakeWhile(i => i != 0).ToArray());
+            
+                if (!CfgMgrName(interface_name, out name, out string instance_name) &&
+                    !HIDProductName(interface_name, out name)
+                ) {
+                    name = instance_name?? interface_name;
+                }
+            }
+
+            return new Source(count, name, interface_name);
         }
 
         public override string ToString() => Name;
